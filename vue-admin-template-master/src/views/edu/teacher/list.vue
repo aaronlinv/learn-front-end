8e3bf4df6a548d5179a7f6b23a1d86dc9e9c6745
Aaron <1546848781@qq.com>
2020-07-15 12:23:20 +0800

    条件查询表单

--- vue-admin-template-master/src/views/edu/teacher/list.vue
+++ vue-admin-template-master/src/views/edu/teacher/list.vue
@@ -4,11 +4,11 @@
   <!--查询表单-->
     <el-form :inline="true" class="demo-form-inline">
       <el-form-item>
-        <el-input v-model="searchObj.name" placeholder="讲师名"/>
+        <el-input v-model="teacherQuery.name" placeholder="讲师名"/>
       </el-form-item>
 
       <el-form-item>
-        <el-select v-model="searchObj.level" clearable placeholder="讲师头衔">
+        <el-select v-model="teacherQuery.level" clearable placeholder="讲师头衔">
           <el-option :value="1" label="高级讲师"/>
           <el-option :value="2" label="首席讲师"/>
         </el-select>
@@ -16,7 +16,7 @@
 
       <el-form-item label="添加时间">
         <el-date-picker
-          v-model="searchObj.begin"
+          v-model="teacherQuery.begin"
           type="datetime"
           placeholder="选择开始时间"
           value-format="yyyy-MM-dd HH:mm:ss"
@@ -25,7 +25,7 @@
       </el-form-item>
       <el-form-item>
         <el-date-picker
-          v-model="searchObj.end"
+          v-model="teacherQuery.end"
           type="datetime"
           placeholder="选择截止时间"
           value-format="yyyy-MM-dd HH:mm:ss"
@@ -33,7 +33,7 @@
         />
       </el-form-item>
 
-      <el-button type="primary" icon="el-icon-search" @click="fetchData()">查询</el-button>
+      <el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
       <el-button type="default" @click="resetData()">清空</el-button>
     </el-form>
 
@@ -122,7 +122,7 @@ export default {
             total: 0,
 
             teacherQuery: {
-
+                // 属性可以不定义，上面使用到了会自动添加
             }
 
         }
