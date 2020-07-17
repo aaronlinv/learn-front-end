1ca20f8fbd1476eb7bfb7372f7dc4741fb4fcb88
Aaron <1546848781@qq.com>
2020-07-17 11:54:14 +0800

    添加路由 和view

--- vue-admin-template-master/src/router/index.js
+++ vue-admin-template-master/src/router/index.js
@@ -77,13 +77,13 @@ export const constantRouterMap = [
       {
         path: 'list',
         name: '课程分类列表',
-        component: () => import('@/views/edu/teacher/list'),
+        component: () => import('@/views/edu/subject/list'),
         meta: { title: '课程分类列表', icon: 'table' }
       },
       {
         path: 'save',
         name: '添加课程分类',
-        component: () => import('@/views/edu/teacher/save'),
+        component: () => import('@/views/edu/subject/save'),
         meta: { title: '添加课程分类', icon: 'tree' }
       }
     ]
