b23e6b1e20f6a9fa6983f38558fd740c12d7e7c3
Aaron <1546848781@qq.com>
2020-07-17 15:34:41 +0800

    对接后端接口

--- vue-admin-template-master/src/views/edu/subject/list.vue
+++ vue-admin-template-master/src/views/edu/subject/list.vue
@@ -15,49 +15,17 @@
 </template>
 
 <script>
+import subject from '@/api/edu/subject'
 export default {
-
   data() {
     return {
       filterText: '',
-      data2: [{
-        id: 1,
-        label: 'Level one 1',
-        children: [{
-          id: 4,
-          label: 'Level two 1-1',
-          children: [{
-            id: 9,
-            label: 'Level three 1-1-1'
-          }, {
-            id: 10,
-            label: 'Level three 1-1-2'
-          }]
-        }]
-      }, {
-        id: 2,
-        label: 'Level one 2',
-        children: [{
-          id: 5,
-          label: 'Level two 2-1'
-        }, {
-          id: 6,
-          label: 'Level two 2-2'
-        }]
-      }, {
-        id: 3,
-        label: 'Level one 3',
-        children: [{
-          id: 7,
-          label: 'Level two 3-1'
-        }, {
-          id: 8,
-          label: 'Level two 3-2'
-        }]
-      }],
+      // 返回分类数据
+      data2: [],
       defaultProps: {
+        //   封装数据的properties名称
         children: 'children',
-        label: 'label'
+        label: 'title'
       }
     }
   },
@@ -66,11 +34,20 @@ export default {
       this.$refs.tree2.filter(val)
     }
   },
-
+created(){
+    this.getAllSubjectList()
+},
   methods: {
+    getAllSubjectList(){
+        subject.getSubjectList()
+        .then(response =>{
+            this.data2 = response.data.list
+        })
+        
+    },
     filterNode(value, data) {
       if (!value) return true
-      return data.label.indexOf(value) !== -1
+      return data.title.indexOf(value) !== -1
     }
   }
 }
