4305a819492d789c11664ef86ce89a5bbfed1a4f
Aaron <1546848781@qq.com>
2020-07-18 10:35:30 +0800

    增加修改和css

--- vue-admin-template-master/src/views/edu/course/chapter.vue
+++ vue-admin-template-master/src/views/edu/course/chapter.vue
@@ -10,18 +10,35 @@
       <el-step title="最终发布"/>
     </el-steps>
 
+  <ul class="chanpterList">
+      <li
+          v-for="chapter in chapterVideoList"
+          :key="chapter.id">
+          <p>
+              {{ chapter.title }}
 
-    <ul>
-      <li v-for="chapter in chapterVideoList" :key="chapter.id">
-          {{chapter.title}}
-          <ul>
-            <li v-for="video in chapter.children" :key="video.id">
-              {{video.title}}
+              <span class="acts">
+                  <el-button type="text">添加课时</el-button>
+                  <el-button style="" type="text">编辑</el-button>
+                  <el-button type="text">删除</el-button>
+              </span>
+          </p>
 
-            </li>
+          <!-- 视频 -->
+          <ul class="chanpterList videoList">
+              <li
+                  v-for="video in chapter.children"
+                  :key="video.id">
+                  <p>{{ video.title }}
+                      <span class="acts">
+                          <el-button type="text">编辑</el-button>
+                          <el-button type="text">删除</el-button>
+                      </span>
+                  </p>
+              </li>
           </ul>
       </li>
-    </ul>
+  </ul>
     <el-form label-width="120px">
 
       <el-form-item>
@@ -78,3 +95,44 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.chanpterList{
+    position: relative;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.chanpterList li{
+  position: relative;
+}
+.chanpterList p{
+  float: left;
+  font-size: 20px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 70px;
+  line-height: 50px;
+  width: 100%;
+  border: 1px solid #DDD;
+}
+.chanpterList .acts {
+    float: right;
+    font-size: 14px;
+}
+
+.videoList{
+  padding-left: 50px;
+}
+.videoList p{
+  float: left;
+  font-size: 14px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 50px;
+  line-height: 30px;
+  width: 100%;
+  border: 1px dotted #DDD;
+}
+
+</style>
