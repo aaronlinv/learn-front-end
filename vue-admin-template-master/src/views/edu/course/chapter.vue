f414ba5fe026f180e9e57f2d7d263ecd799beff6
Aaron <1546848781@qq.com>
2020-07-19 18:03:48 +0800

    上传视频

--- vue-admin-template-master/src/views/edu/course/chapter.vue
+++ vue-admin-template-master/src/views/edu/course/chapter.vue
@@ -83,9 +83,30 @@
         <el-radio :label="false">默认</el-radio>
       </el-radio-group>
     </el-form-item>
-    <el-form-item label="上传视频">
-      <!-- TODO -->
-    </el-form-item>
+
+
+  <el-form-item label="上传视频">
+      <el-upload
+            :on-success="handleVodUploadSuccess"
+            :on-remove="handleVodRemove"
+            :before-remove="beforeVodRemove"
+            :on-exceed="handleUploadExceed"
+            :file-list="fileList"
+            :action="BASE_API+'/eduvod/video/uploadAlyiVideo'"
+            :limit="1"
+            class="upload-demo">
+      <el-button size="small" type="primary">上传视频</el-button>
+      <el-tooltip placement="right-end">
+          <div slot="content">最大支持1G，<br>
+              支持3GP、ASF、AVI、DAT、DV、FLV、F4V、<br>
+              GIF、M2T、M4V、MJ2、MJPEG、MKV、MOV、MP4、<br>
+              MPE、MPG、MPEG、MTS、OGG、QT、RM、RMVB、<br>
+              SWF、TS、VOB、WMV、WEBM 等视频格式上传</div>
+          <i class="el-icon-question"/>
+      </el-tooltip>
+      </el-upload>
+  </el-form-item>
+
   </el-form>
   <div slot="footer" class="dialog-footer">
     <el-button @click="dialogVideoFormVisible = false">取 消</el-button>
@@ -121,7 +142,11 @@ export default {
 
       },
       // Video弹窗
-      dialogVideoFormVisible:false
+      dialogVideoFormVisible:false,
+
+      // 视频
+      fileList: [],//上传文件列表
+      BASE_API: process.env.BASE_API // 接口API地址
     }
   },
 
@@ -140,6 +165,15 @@ export default {
 
   methods: {
 
+    // =========================视频=========================
+    // 上传成功
+    handleVodUploadSuccess(response, file, fileList){
+        this.video.videoSourceId = response.data.videoId
+    },
+    // 上传之前
+    handleUploadExceed(){
+        this.$message.warning('想要重新上传视频，请先删除已上传的视频')
+    },
     // =========================小节=========================
     // 小节弹窗
     removeViedo(id){
