add35ca4e12a661b1281af49d2ecc8d9c84eceab
Aaron <1546848781@qq.com>
2020-07-26 16:31:29 +0800

    课程播放

---
+++ nuxt/pages/player/_vid.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+
+    <!-- 阿里云视频播放器样式 -->
+    <link rel="stylesheet" href="https://g.alicdn.com/de/prismplayer/2.8.1/skins/default/aliplayer-min.css" >
+    <!-- 阿里云视频播放器脚本 -->
+    <script charset="utf-8" type="text/javascript" src="https://g.alicdn.com/de/prismplayer/2.8.1/aliplayer-min.js" />
+
+    <!-- 定义播放器dom -->
+    <div id="J_prismPlayer" class="prism-player" />
+  </div>
+</template>
+<script>
+import vod from '@/api/vod'
+
+export default {
+    laout:'video' ,// 使用布局
+    asyncData({params,error}){
+        return vod.getPlayAuth(params.vid) // vid 对应 _vid.vue
+            .then(response =>{
+                return {
+                    playAuth:response.data.data.playAuth,
+                    vid:params.vid
+                }
+            })
+        
+    },
+    // 因为使用了异步调用，页面created的时候，数据还没请求
+    mounted(){// 页面渲之后
+         new Aliplayer({
+            id: 'J_prismPlayer',
+            vid: this.vid, // 视频id
+            playauth: this.playAuth, // 播放凭证
+            encryptType: '1', // 如果播放加密视频，则需设置encryptType=1，非加密视频无需设置此项
+            width: '100%',
+            height: '500px'
+        }, function(player) {
+            console.log('播放器创建成功')
+        })
+    }
+
+}
+</script>
