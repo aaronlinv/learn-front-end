c79dd0097e690c38b35cf6f4f024146f3edb300f
Aaron <1546848781@qq.com>
2021-02-28 14:59:23 +0800

    细节修改

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: '谷粒学院前台网站',
+    title: '橙汁学院',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
